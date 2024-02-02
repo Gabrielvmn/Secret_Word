4f9ae1729cd327639ee946db6a0cd5c73868009e
Gabrielvmn <gabriel_tdl@hotmail.com>
2024-02-01 22:04:30 -0300

    first commit

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Maçã", "Melancia", "Mamão", "Banana", "Tomate"],
+    corpo: ["Cabelo", "Unha", "Cabeça", "Pescoço", "Mão"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador"],
+    programação: ["HTML", "Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
